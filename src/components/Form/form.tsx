94e6ec1ce119412e787daed4e86dc4dcffba112a
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-01-19 15:48:19 +0200

    add forms

---
+++ src/components/Form/form.tsx
@@ -0,0 +1,10 @@
+// import css from "./form.module.css";
+
+// export default function Form(submit) {
+//   return (
+//     <form className={css.FormStyle} onSubmit={submit}>
+//       <input type="text" name="username" />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
